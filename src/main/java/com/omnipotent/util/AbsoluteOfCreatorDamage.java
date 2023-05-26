22179958c53b834850872b40394ee43042a1fbf6
GANGS <75382609+gamerYToff@users.noreply.github.com>
2023-05-25 22:02:51 -0300

    Corrected numbers and new effects added to death.

--- src/main/java/com/omnipotent/util/AbsoluteOfCreatorDamage.java
+++ src/main/java/com/omnipotent/util/AbsoluteOfCreatorDamage.java
@@ -7,6 +7,7 @@ import net.minecraft.util.EntityDamageSource;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.util.text.TextFormatting;
 
 import javax.annotation.Nullable;
 
@@ -18,12 +19,13 @@ public class AbsoluteOfCreatorDamage extends EntityDamageSource {
     @Override
     public ITextComponent getDeathMessage(EntityLivingBase entity) {
         ItemStack itemstack = damageSourceEntity instanceof EntityLivingBase ? ((EntityLivingBase) damageSourceEntity).getHeldItem(EnumHand.MAIN_HAND) : null;
-        String s = "MORTO POR S@#@#@# ABSOLUTE OF CREATOR";
-        int rando = entity.getEntityWorld().rand.nextInt(5);
-        if (rando != 0) {
-            s = s + "." + rando;
-        }
-        return new TextComponentTranslation(s, entity.getDisplayName(), itemstack.getDisplayName());
+        TextFormatting darkPurple = TextFormatting.DARK_PURPLE;
+        String fallen = ""+TextFormatting.BLACK+TextFormatting.ITALIC+TextFormatting.OBFUSCATED;
+        TextFormatting red = TextFormatting.RED;
+        TextFormatting darkRed = TextFormatting.DARK_RED;
+        String namePlayerDead = entity.getDisplayName().getFormattedText();
+        String s = darkRed+namePlayerDead+darkPurple+" MORTO POR " +fallen+ " S@#@#@#"+ TextFormatting.GRAY +" ABSOLUTE OF CREATOR";
+        return new TextComponentTranslation(s, namePlayerDead, itemstack.getDisplayName());
     }
 
 }
