eb6c9b36fcc6b83292f742dce2fdbcb5ff90d50e
GANGS <75382609+gamerYToff@users.noreply.github.com>
2023-05-29 17:05:41 -0300

    two new constants counterAttack and attackYourWolf

--- src/main/java/com/omnipotent/tools/KaiaConstantsNbt.java
+++ src/main/java/com/omnipotent/tools/KaiaConstantsNbt.java
@@ -2,6 +2,7 @@ package com.omnipotent.tools;
 
 public class KaiaConstantsNbt {
     public static final String blockBreakArea = "areaBloco";
+    public static final String counterAttack = "counterAttack";
     public static final String killAllEntities = "killAllEntities";
     public static final String rangeAttack = "rangeAttack";
     public static final String killFriendEntities = "killFriendEntities";
@@ -9,5 +10,6 @@ public class KaiaConstantsNbt {
     public static final String ownerID = "ownerID";
     public static final String PositionKaiaEntityItem = "PositionKaiaEntityItem";
     public static final String idLigation = "idLigation";
+    public static final String attackYourWolf = "attackYourWolf";
 
 }
