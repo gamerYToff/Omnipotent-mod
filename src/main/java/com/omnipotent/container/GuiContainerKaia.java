d4fcfd181b7afb8bbff45724b44c8ca9d8c9b1ab
GANGS <75382609+gamerYToff@users.noreply.github.com>
2023-05-29 17:01:25 -0300

    containers in test

---
+++ src/main/java/com/omnipotent/container/GuiContainerKaia.java
@@ -0,0 +1,18 @@
+package com.omnipotent.container;
+
+import com.omnipotent.network.NetworkRegister;
+import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.inventory.Container;
+
+public class GuiContainerKaia extends GuiContainer {
+    public GuiContainerKaia(Container inventorySlotsIn) {
+        super(inventorySlotsIn);
+    }
+
+    @Override
+    protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
+    }
+    @Override
+    protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
+    }
+}
