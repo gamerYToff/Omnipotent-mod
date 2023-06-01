0747a87d1cab022a39495f59e64d20fe2e381242
GANGS <75382609+gamerYToff@users.noreply.github.com>
2023-05-31 21:39:25 -0300

    crash fixed

--- src/main/java/com/omnipotent/mixin/MixinEntity.java
+++ src/main/java/com/omnipotent/mixin/MixinEntity.java
@@ -3,11 +3,9 @@ package com.omnipotent.mixin;
 import com.omnipotent.util.KaiaUtil;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.server.management.PlayerList;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
-import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import org.spongepowered.asm.mixin.Mixin;
@@ -17,7 +15,6 @@ import org.spongepowered.asm.mixin.Shadow;
 import javax.annotation.Nullable;
 
 import static com.omnipotent.tools.KaiaConstantsNbt.interactLiquid;
-
 
 @Mixin(Entity.class)
 public abstract class MixinEntity {
@@ -36,7 +33,6 @@ public abstract class MixinEntity {
     @Shadow
     private int entityId;
 
-
     @Shadow
     public abstract String getName();
 
@@ -44,15 +40,19 @@ public abstract class MixinEntity {
      * @author
      * @reason
      */
+    @Overwrite
     @Nullable
     @SideOnly(Side.CLIENT)
-    @Overwrite
     public RayTraceResult rayTrace(double blockReachDistance, float partialTicks) {
         boolean stopOnLiquid = false;
-        if((EntityPlayer) (Object) this instanceof EntityPlayer){
-            EntityPlayer player = (EntityPlayer) (Object) this;
-            if (player != null && KaiaUtil.withKaiaMainHand(player)) {
-                stopOnLiquid = KaiaUtil.getKaiaInMainHand(player).getTagCompound().getBoolean(interactLiquid);
+        if (this != null) {
+            if ((EntityPlayer) (Object) this instanceof EntityPlayer) {
+                EntityPlayer player = (EntityPlayer) (Object) this;
+                if (player != null && KaiaUtil.withKaiaMainHand(player)) {
+                    if (KaiaUtil.getKaiaInMainHand(player).getTagCompound() != null) {
+                        stopOnLiquid = KaiaUtil.getKaiaInMainHand(player).getTagCompound().getBoolean(interactLiquid);
+                    }
+                }
             }
         }
         Vec3d vec3d = this.getPositionEyes(partialTicks);
