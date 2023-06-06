8ebbfef58b2c62b3cc0a8b7bd2f8b2da269d72f6
GANGS <75382609+gamerYToff@users.noreply.github.com>
2023-06-05 22:03:35 -0300

    MixinOnDeathPlayerMp now extends EntityPlayer

--- src/main/java/com/omnipotent/mixin/MixinOnDeathPlayerMp.java
+++ src/main/java/com/omnipotent/mixin/MixinOnDeathPlayerMp.java
@@ -1,21 +1,25 @@
 package com.omnipotent.mixin;
 
+import com.mojang.authlib.GameProfile;
 import com.omnipotent.util.KaiaUtil;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.network.NetHandlerPlayServer;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 
 @Mixin(EntityPlayerMP.class)
-public abstract class MixinOnDeathPlayerMp extends EntityLivingBase {
-
-    public MixinOnDeathPlayerMp(World worldIn) {
-        super(worldIn);
+public abstract class MixinOnDeathPlayerMp extends EntityPlayer {
+    public MixinOnDeathPlayerMp(World worldIn, GameProfile gameProfileIn) {
+        super(worldIn, gameProfileIn);
     }
 
     @Inject(method = "onDeath", at = @At("HEAD"))
